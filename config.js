5383e48f0f4ded777f4c0a6af98fe0ff0a681e55
Eva Castelo <eva.castelo@outlook.com>
2015-01-30 21:26:53 -0500

    configurations

--- config.js
+++ config.js
@@ -19,7 +19,8 @@ config = {
 		host: process.env.POSTGRES_HOST,
 		user: process.env.POSTGRES_USER,
 		password: process.env.POSTGRES_PASSWORD,
-		database: process.env.POSTGRES_DATABASE
+		database: process.env.POSTGRES_DATABASE,
+		port: 5432
             },
             debug: false
         },
